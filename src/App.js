b4730394102e88c6d3f6162a914eb270843d4bdd
chiaraalicata <74967879+chiaraalicata@users.noreply.github.com>
2022-05-05 18:28:21 +0200

    created base for the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="San Francisco" />
+        <footer>
+          Project coded by{" "}
+          <a
+            href="#"
+            target="_blank"
+            title="visit Chiara´s coding portfolio"
+            rel="noopener noreferrer"
+          >
+            Chiara Alicata
+          </a>{" "}
+          and this is{" "}
+          <a
+            href="#"
+            title="open-source link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open Sourced on GitHub
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
